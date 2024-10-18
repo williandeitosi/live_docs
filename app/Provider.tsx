ad12f092a334ffbfb0e084600077bd8533463f7e
williandeitosi <williandeitosi@gmail.com>
2024-10-18 20:36:41 -0300

    feat: create `room.actions.ts`, `user.actions.ts`, `ActiveCollaborators.tsx`, `AddDocumentBtn.tsx` and `AddDocumentBtn.tsx`

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Loader from "@/components/Loader";
+import { getClerkUsers } from "@/lib/actions/user.actions";
 import {
   ClientSideSuspense,
   LiveblocksProvider,
@@ -9,7 +10,14 @@ import type { ReactNode } from "react";
 
 const Provider = ({ children }: { children: ReactNode }) => {
   return (
-    <LiveblocksProvider authEndpoint="/api/liveblocks-auth">
+    <LiveblocksProvider
+      authEndpoint="/api/liveblocks-auth"
+      resolveUsers={async ({ userIds }) => {
+        const users = await getClerkUsers({ userIds });
+
+        return users;
+      }}
+    >
       <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>
     </LiveblocksProvider>
   );
