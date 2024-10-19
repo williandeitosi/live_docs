402ce2b05c98646036805677da529571a1e4a1d7
williandeitosi <williandeitosi@gmail.com>
2024-10-19 20:56:41 -0300

    fix: bugs

--- components/UserTypeSelector.tsx
+++ components/UserTypeSelector.tsx
@@ -13,7 +13,9 @@ const UserTypeSelector = ({
 }: UserTypeSelectorParams) => {
   const accessChangeHandler = (type: UserType) => {
     setUserType(type);
-    onClickHandler && onClickHandler(type);
+    if (onClickHandler) {
+      onClickHandler(type);
+    }
   };
 
   return (
